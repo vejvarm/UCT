c5116c701b3b4b393d12602ef2415e748a41f8fb
Martin Vejvar <vejvarm@gmail.com>
2018-10-17 13:44:37 +0200

    PMR least squares

--- PMR/4/MNC/mnc.m
+++ PMR/4/MNC/mnc.m
@@ -1,24 +1,26 @@
 function [B,A] = mnc(nb,na,y,u)
     
     % sestrojeni matice X
-    X = zeros(length(u)-na,nb + na);
-    
+    X = zeros(length(u)-na,nb + na - 1);
+%     X = zeros(nb+na);
+
     for i = na:length(u)
-        X(i-na+1,:) = [u(i:-1:i-nb)' y(i-1:-1:i-na+1)'];
+        X(i,:) = [u(i:-1:i-nb+1)' -y(i-1:-1:i-na+1)'];
     end
     
-    X
-    max(X'*X)
-    min(X'*X)
-    det(X'*X)
-    eig(X'*X)
+    length(u(i:-1:i-nb))
+    length(y(i-1:-1:i-na+1))
+%     X
     
     % vypocet koeficientu
-    K = (X'*X)\X'*y(na:end);
+    K = (X'*X)\X'*y;
+%     K = pinv(X)*y(na:end);
+%     K = y(na:end)*pinv(X);
+%     K = lsqminnorm(X,y(na:end),'warn');
     
     % osamostatneni B a A
     B = K(1:nb);
-    A = K(nb+1:end);
+    A = [1; K(nb+1:end)];
     
     
     
