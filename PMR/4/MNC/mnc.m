4f3732c95c98c9b5b0022de49d01fb84bc4907dc
Martin Vejvar <vejvarm@gmail.com>
2018-12-12 13:59:01 +0100

    update: add least squares implementation

--- PMR/4/MNC/mnc.m
+++ PMR/4/MNC/mnc.m
@@ -3,7 +3,7 @@ function [B,A] = mnc(nb,na,y,u)
     % sestrojeni matice X
     X = zeros(length(u)-na,nb + na - 1);
 
-    % naplneni matice x
+    % naplneni matice X
     for i = na:length(u)
         X(i,:) = [u(i:-1:i-nb+1)' -y(i-1:-1:i-na+1)'];
     end
